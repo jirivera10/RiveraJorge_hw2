040480254d3db87b382cc3f8cc9ba56491b69622
Jorge Rivera <ji.rivera10@uniandes.edu.co>
2019-04-18 08:36:31 -0500

    Se realizan los espectogramas

--- Fourier.py
+++ Fourier.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pylab as plt
+from scipy.fftpack import fft, fftfreq
 #Lectura de datos
 signal1=np.genfromtxt('signal.dat')
 tiempo1=signal1[:,0]
@@ -28,28 +29,39 @@ for h in range(0,N):
     a=0
     for i in range(0,N):
         a+=signal[i]*np.exp(-1j*2*np.pi*i*(h/N))
-    fourier1.append(a)    
+    fourier1.append(a)
 fourier = np.fft.fft(signal)
 n = signal.size
 timestep = tiempo1[1]-tiempo1[0]
 freq = np.fft.fftfreq(n, d=timestep)
 plt.plot(freq,fourier1,color='indigo')
 plt.title('Trasformada de Fourier señales')
+plt.xlabel(u'Tiempo (s)')
+plt.ylabel(u'Fourier')
 plt.savefig('Fourier_trans1.pdf')
 plt.show()
-#Transformada de suma 
+#Transformada de suma
 N=len(tiempo2)
 fourier2=[]
 for h in range(0,N):
     b=0
     for i in range(0,N):
         b+=Suma[i]*np.exp(-1j*2*np.pi*i*(h/N))
-    fourier2.append(b)    
+    fourier2.append(b)
 fourierSuma = np.fft.fft(Suma)
 n = Suma.size
 timestep = tiempo2[1]-tiempo2[0]
 freqSuma = np.fft.fftfreq(n, d=timestep)
 plt.plot(freqSuma,fourier2,color='firebrick')
 plt.title('Trasformada de Fourier suma señales')
+plt.xlabel(u'Tiempo (s)')
+plt.ylabel(u'Fourier')
 plt.savefig('Fourier_transSuma.pdf')
 plt.show()
+#Espectograma de las dos señales aprendí a hacer el specgram con https://www.pybonacci.org/2012/09/29/transformada-de-fourier-discreta-en-python-con-scipy/
+import numpy as np
+import matplotlib.pylab as plt
+Pxx, freqs, bins, im = plt.specgram(signal, NFFT=256, Fs=2)
+plt.colorbar(im).set_label(u'')
+plt.xlabel(u'Tiempo (s)')
+plt.ylabel(u'Señales')
