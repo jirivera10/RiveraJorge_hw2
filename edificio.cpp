d14271f8c1263ab6fe82c5d2715051cbbc21fab6
Jorge Rivera <ji.rivera10@uniandes.edu.co>
2019-05-08 22:23:05 -0500

    Se crea el archivo edificio.cpp con las constantes de masa, gamma ...

---
+++ edificio.cpp
@@ -0,0 +1,12 @@
+#include <cmath>
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+int main( ){
+  float m = 1000.0;
+  float k = 2000.0;
+  float gamma = 0.0;
+  float dt = 0.05;
+  reurn 0;
+}
